40696cfbaffec102d9d7e22a38d33245664bb07e
Meraki_DNA <dononichols@gmail.com>
2022-10-07 17:23:03 -0400

    javaScript-training

---
+++ app.js
@@ -0,0 +1,70 @@
+// ----------------
+// HELPER METHODS
+// 
+// document.write('hello world');
+// alert('hello world');
+// console.log('hello world');
+// ----------------
+
+// VID. 27 (CHALLENGE - VARIABLES #1)
+/*
+var firstName = "Don";
+var last_name = "Nichols";
+console.log(firstName, last_name);
+
+var address = "Mains St.";
+address = "First St.";
+console.log(address);
+*/
+
+// -----------------------
+
+// VID. 31 (CHALLENG - STRINGS + CONCATINATION #2)
+/*
+var street = "main street";
+var country = "America";
+
+var fullMailingAddress = street + ' ' + country;
+console.log(fullMailingAddress);
+*/
+
+// ------------------------
+
+// Number | Operators
+
+// +=, -=, /=, *=, ++, --, %
+// Modulus (%) operator returns the remainder after integer division
+/*
+// To get the Modulus of:
+const slices = 10;
+const children = 3;
+const amount = slices % children;
+
+console.log(amount);
+*/
+
+// ----------------------------
+
+// VID. 33 (CHALLENG - NUMBERS #3)
+/*
+var score1 = 20;
+var score2 = 50;
+var score3 = 11;
+
+const total = score1 + score2 + score3;
+var average = total / 3;
+
+console.log(total);
+console.log(average);
+// VID. 34 (CHALLENG - NUMBERS #3)
+var plates = 20;
+var people = 7;
+
+var platesRemaining = plates % people;
+platesRemaining++;
+var message = "There are " + platesRemaining + " plates available."
+
+console.log(message);
+*/
+// ------------------------------
+
