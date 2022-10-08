05568496e47aa208c1cfcd7d738d1d05a8c002fd
Meraki_DNA <dononichols@gmail.com>
2022-10-08 15:14:42 -0400

    -arrays training

--- app.js
+++ app.js
@@ -68,3 +68,49 @@ console.log(message);
 */
 // ------------------------------
 
+// ======================
+// ARRAYS
+
+// Example:
+// instead of creating individual variables; we can use an Array
+/*
+const friend1 = 'john';
+const friend2 = 'peter';
+const friend3 = 'bob';
+const friend4 = 'susy';
+const friend5 = 45;
+const friend6 = undefined;
+const friend7 = null;
+*/
+/*
+const friends = ['john','peter','bob','susy',45,'undefined','null'];
+
+let betstFriend = friends [2];
+console.log(betstFriend); // displays: bob
+
+console.log(friends [3]); // displays: susy
+
+// re-assign a new value to an index:
+// change index [1] peter => to mike
+friends [1] = 'mike';
+//console.log(friends);
+*/
+// VID. 38 ( CHALLENGE #4 - FRUITS ARRAY )
+
+// 1. create "fruits" array and store some fruit values
+// 2. setup the last item as number (random)
+// 3. assign first fruit to the variable
+// 4. re-assign last array item to the actual fruit
+// 5. log both first fruit variable and entire fruits array
+/*
+const fruits = ['apple','orange','pear','lemon','35'];
+
+const firstFruit = fruits[0];
+
+fruits[4] = 'grape';
+console.log(firstFruit, fruits);
+*/
+
+// ==========================
+
+// FUNCTIONS - Declare, Invoke
