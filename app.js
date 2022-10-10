dd486ef946863dd063e009af4a07507ab107f37d
Meraki_DNA <dononichols@gmail.com>
2022-10-10 12:03:24 -0400

    -Functions training

--- app.js
+++ app.js
@@ -171,7 +171,7 @@ console.log(dimensions);
 // expressions- another way to define a function
 // create a variable, assign to FUNCTIOn (not value), use var
 // diff - hoisting, use - arrow func, libraries
-
+/*
 // function definition/declaration
 function addValues(num1, num2) {
     return num1 + num2;
@@ -180,6 +180,48 @@ function addValues(num1, num2) {
 const firstValue = addValues(3, 4);
 const secondValue = addValues(12, 34);
 // function expression
+const add = function (num1, num2) {
+    return num1 + num2;
+};
 
-const values = [firstValue, secondValue];
+// const thirdValue = add(5, 6);
+const values = [firstValue, secondValue, add(5, 6)];
 console.log(values);
+
+// example of a simple Arrow Function
+// const multiply = (num1, num2) => num1 * num2;
+//
+*/
+
+//--------------
+// FUNCTION ( CHALLENGE #5 )
+/*
+
+1. create "calculateTotal" function
+2. add two parameters subTotal, tax
+3. return sum of parameters
+
+4. create 3 vars "order1", "order2", "order3"
+5. call calculateResults, pass in some values and assign results to each order
+6. log all three orders
+7. refactor "calculateTotal" to funtion expression
+
+*/
+// Steps 1-3
+function calculateTotal(subTotal, tax)
+// the above refactored:
+// const calculateTotal = function random(subTotal, tax)
+{
+    // const total = subTotal + tax;
+    // return total;
+    // -- another way to write the above:
+    return subTotal + tax;
+}
+//console.log(calculateTotal(200, 20));
+
+// Steps 4-7
+const order1 = calculateTotal(100, 10);
+const order2 = calculateTotal(50, 5);
+const order3 = calculateTotal(25, 5);
+
+console.log(order1, order2, order3);
