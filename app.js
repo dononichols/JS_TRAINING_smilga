397665e535bd68e7775b3ec0b682491b6efd7763
Meraki_DNA <dononichols@gmail.com>
2022-10-25 18:07:44 -0400

    REDUCE array overview

--- app.js
+++ app.js
@@ -958,7 +958,7 @@ console.log(developer);
 //returns first match
 // if No Match, it returns - undefined
 // great for getting unique value
-
+/*
 const people = [
     { name: 'bob', age: 20, position: 'developer', id: 1 },
     { name: 'peter', age: 25, position: 'designer', id: 2 },
@@ -970,3 +970,35 @@ const person = people.find(function(person) {
     return person.id === 3;
 });
 console.log(person);
+*/
+
+// ----------------------------------
+// VID.75
+
+// --- REDUCE array --- reduce method
+
+// iterates, callback function
+// reduces to a single value - number, array, object
+// 1 parameter ('acc') - total of all calculations
+// 2 parameter ('curr') - current iteration/value
+
+/*
+// calculate how much my team cost me daily
+const people = [
+    { name: 'bob', age: 20, position: 'developer', id: 1, salary:200 },
+    { name: 'peter', age: 25, position: 'designer', id: 2, salary:300  },
+    { name: 'susy', age: 30, position: 'the boss', id: 3, salary:500  },
+    { name: 'anna', age: 35, position: 'the boss', id: 4, salary:500  },
+];
+
+const total = people.reduce(function(acc,currItem){
+    console.log(`total ${acc}`);
+    console.log(`current money : ${currItem.salary}`);
+    acc += currItem.salary;
+    // you want to return the accumilator, or you'll get bugs
+    return acc;
+},0);
+
+console.log(total);
+*/
+//
