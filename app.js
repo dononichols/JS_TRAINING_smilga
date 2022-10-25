153637f84ea7a5c9212b6491c3fc05fad1c42e0e
Meraki_DNA <dononichols@gmail.com>
2022-10-25 17:12:15 -0400

    -FIND Array overview

--- app.js
+++ app.js
@@ -926,7 +926,7 @@ console.log(newPeople);
 // Does Return a NEW Array
 // can manipulate the size of the new array
 // returns based on condition
-
+/*
 const people = [
     { name: 'bob', age: 20, position: 'developer'},
     { name: 'peter', age: 25, position: 'designer'},
@@ -947,3 +947,26 @@ const developer = people.filter(function(person) {
     return person.position === 'developer';
 });
 console.log(developer);
+*/
+
+// ---------------------------------
+// VID.74
+
+// --- FIND array ----
+
+// returns single instance - in this case, an OBJECT
+//returns first match
+// if No Match, it returns - undefined
+// great for getting unique value
+
+const people = [
+    { name: 'bob', age: 20, position: 'developer', id: 1 },
+    { name: 'peter', age: 25, position: 'designer', id: 2 },
+    { name: 'susy', age: 30, position: 'the boss', id: 3 },
+    { name: 'anna', age: 35, position: 'the boss', id: 4 },
+];
+
+const person = people.find(function(person) {
+    return person.id === 3;
+});
+console.log(person);
