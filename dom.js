b4317c271745a41cca81f762091546bd9a849993
Meraki_DNA <dononichols@gmail.com>
2022-10-30 18:35:29 -0400

    -Get Element By ID overview. v.91

---
+++ dom.js
@@ -0,0 +1,23 @@
+// window object = browser api
+// document
+// console.dir
+
+// -----------------------------------
+//vid.91
+
+// --- Get Element By ID ---
+
+// Select the element or group of elements that we want
+// Decide the effect we want to apply to the selection
+
+// getElementById('element')
+
+const h1 = document.getElementById('title');
+h1.style.color = 'red';
+
+const btn = document.getElementById('btn');
+
+btn.style.backgroundColor = 'blue';
+btn.style.color = 'white';
+
+// -------------------------------------
