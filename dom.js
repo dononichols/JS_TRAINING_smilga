350374af7167a43d5ec3be2a8f195b6248017263
Meraki_DNA <dononichols@gmail.com>
2022-10-31 11:12:05 -0400

    -Get Elements By Tag Name v.92

--- dom.js
+++ dom.js
@@ -11,7 +11,7 @@
 // Decide the effect we want to apply to the selection
 
 // getElementById('element')
-
+/*
 const h1 = document.getElementById('title');
 h1.style.color = 'red';
 
@@ -19,5 +19,38 @@ const btn = document.getElementById('btn');
 
 btn.style.backgroundColor = 'blue';
 btn.style.color = 'white';
-
+*/
 // -------------------------------------
+//vid.92
+
+// --- Get Element By Tag Name ---
+
+// Select the element or group of elements we want
+// Decide the effect we want to apply to the selection
+
+// get ElementsByTagName('tagname');
+//HTMLCollection = array-like object
+// index, length property but not array methods (forEach)
+
+// querySelectorAll() in most cases -returns a none list
+// NodeList - objects are collections of nodes
+// can run forEach
+// turn them into array - spread operator [...]
+// after that can use any array properties
+
+const headings = document.getElementsByTagName('h2');
+headings[0].style.color = 'red';
+// console.log(headings.length);
+
+const items = document.getElementsByTagName('li');
+items[2].style.color = 'orange';
+const betterItems = [ ...items]
+
+betterItems.forEach(function(item){
+    console.log(item);
+});
+
+console.log(items);
+console.log(betterItems);
+
+// ----------------------------------------
