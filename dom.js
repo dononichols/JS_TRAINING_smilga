9269b55f994e94dd20cf806a441f6fe57c2fc09f
Meraki_DNA <dononichols@gmail.com>
2022-11-07 15:32:16 -0500

    -Function Reference..add and remove class. v.111

--- dom.js
+++ dom.js
@@ -436,7 +436,7 @@ random.classList.add('title'); // using this method, we'r able to add all styles
 // select element
 // addEventListener()
 // what event, what to do
-
+/*
 // select the button
 const btn = document.querySelector('.btn');
 const heading = document.querySelector('h2');
@@ -444,5 +444,28 @@ const heading = document.querySelector('h2');
 btn.addEventListener('click',function(){
     heading.classList.add('red');
 })
+
+*/
+// ---------------------------------
+// vid.111
+
+// Function Reference
+
+const btn = document.querySelector('.btn');
+const heading = document.querySelector('h2');
+
+// add and remove the 'red' class to change colors
+function changeColors() {
+  let hasClass = heading.classList.contains('red');
+  if(hasClass){
+    heading.classList.remove('red');
+  }
+  else {
+    heading.classList.add('red');
+  }
+
+}
+
+btn.addEventListener('click', changeColors);
 
 // ---------------------------------
