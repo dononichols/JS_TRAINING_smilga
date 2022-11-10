0528f54fa8b18afe20e5274386902b49b80e2320
Meraki_DNA <dononichols@gmail.com>
2022-11-10 17:48:03 -0500

    - Event - "scroll" ...v.125

--- dom.js
+++ dom.js
@@ -880,3 +880,28 @@ window.addEventListener('load', function () {
 **** Look into more....
 */
 // -----------------------------------
+//vid.125
+
+// --- Event - "scroll"
+
+/*
+
+Scroll Event - on element or document
+
+window.addEventListener('scroll', function() {
+    console.log(window.scrollY + 'px');
+    console.log(window.scrollX + 'px');
+});
+
+document
+
+scrollY - returns the number of pixels the document is currently scrolled along the vertical axis (pageYOffset)
+
+scrollX - returns the number of pixels scrolled along the horizontal axis (pageXOffset)
+*/
+
+// this needs the image from the previous (vid.124) section to work
+window.addEventListener('scroll', function () {
+    console.log(window.screenY + 'px');
+    console.log(window.screenX + 'px');
+});
